3da860351894fee69c86d2acab0e29f85e5a1546
Victoria Shafer <victoriamshafer@gmail.com>
2018-06-05 20:03:37 -0500

    Change the images to the theme.

--- wp-content/themes/cotn/header.php
+++ wp-content/themes/cotn/header.php
@@ -12,7 +12,7 @@
 <body>
     <header>
         <div class="fixed-menu-bar register-wrapper">
-            <img src="<?php bloginfo('template_url') ?>/images/large_logo.svg" alt="Compass Rose" class="compass-rose">
+            <img src="<?php bloginfo('template_url') ?>/images/large_logo.gif" alt="Compass Rose" class="compass-rose">
             <?php
                 wp_nav_menu($arg = array (
                     'menu_class' => 'main-navigation',
