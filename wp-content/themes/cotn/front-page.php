cea1ce08f344c5a1849e994ec3be4e5d27bfd55f
Victoria Shafer <victoriamshafer@gmail.com>
2018-06-06 19:39:37 -0500

    Refine the main title call out block.

--- wp-content/themes/cotn/front-page.php
+++ wp-content/themes/cotn/front-page.php
@@ -5,21 +5,25 @@
 
 <main class="main-content">
     <div class="main-title">
-        <img src="<?php bloginfo('template_url') ?>/images/cotn-smaller.png" alt="Board Game" class="board-game">
-        <div class="title-wrapper center-text">
-            <img src="<?php bloginfo('template_url') ?>/images/new_logo_2019.svg" alt="Compass Rose" class="title-image">
-            <span class="title-text">Twin Cities Longest Running Games Convention.</span>
-                <div>
-                    <p>February 15-17, 2019<br>
-                    Friday Noon to 2 AM<br>
-                    Saturday 8 AM to 2 AM<br>
-                    Sunday 8 AM to 10 PM<br>
-                    Crowne Plaza Minneapolis West</p>
+        <div class="main-title-wrapper">
+            <div class="main-title-left">
+                <div class="main-title-left-content">
+                    <img src="<?php bloginfo('template_url') ?>/images/new_logo_2019.svg" alt="Compass Rose" class="title-image">
+                    <span class="title-text">Twin Cities Longest Running Games Convention.</span>
+                        <div>
+                            <p>February 15-17, 2019<br>
+                            Friday Noon to 2 AM<br>
+                            Saturday 8 AM to 2 AM<br>
+                            Sunday 8 AM to 10 PM<br>
+                            Crowne Plaza Minneapolis West</p>
+                        </div>
+                    <div class="center-text">
+                        <div class="register-now btn">Register Now</div>
+                    </div>
                 </div>
-            <div class="center-text">
-                <div class="register-now btn">Register Now</div>
             </div>
         </div>
+        <img src="<?php bloginfo('template_url') ?>/images/cotn-smaller.png" alt="Board Game" class="board-game">
     </div>
     <div class="home-column-wrapper">
         <?php get_sidebar('home-left'); ?>
